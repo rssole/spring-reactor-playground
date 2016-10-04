82fdd55e2d0e6f1369d7b2b1b264e1c55a4c4e8d
rastko <rssole@gmail.com>
2016-10-05 00:24:34 +0200

    Adding Spring Boot tests

--- src/test/java/org/rastko/reactive/spring/engine/ReactiveEngineIT.java
+++ src/test/java/org/rastko/reactive/spring/engine/ReactiveEngineIT.java
@@ -1,11 +1,22 @@
 package org.rastko.reactive.spring.engine;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.rastko.reactive.spring.PlaygroundApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.bus.EventBus;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PlaygroundApplication.class)
 public class ReactiveEngineIT {
 
+    @Autowired
+    private EventBus bus;
+
+    @Test
+    public void busShouldBeAvailable() {
+        // TODO: Write some test :)
+    }
 }
